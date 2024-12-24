45760548bac753a21cb877758e8c8787e8e22b81
Wafiy Anwarul HIkam <95902360+wafiyanwarul@users.noreply.github.com>
2024-12-24 13:38:38 +0700

    configuration mongo db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 require('dotenv').config()
 
 const app = express()
@@ -37,3 +38,13 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`)
 })
+
+// Koneksi ke MongoDB Atlas
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Terhubung ke MongoDB Atlas');
+}).catch((error) => {
+  console.error('Koneksi ke MongoDB gagal:', error.message);
+});
